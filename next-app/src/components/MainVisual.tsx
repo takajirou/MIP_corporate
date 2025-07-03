ea8017039a4f7210ae79bb74a8355b9684d25574
kanayakaisei <kanayakaisei0903@gmail.com>
2025-07-03 10:51:06 +0900

    MainVisual一旦完成

--- next-app/src/components/MainVisual.tsx
+++ next-app/src/components/MainVisual.tsx
@@ -9,17 +9,17 @@ interface MainVisualProps{
 const MainVisual = ({ title, text, src }:MainVisualProps) => {
     return(
         <div className="titleWrap h-[450px] bg-gradient-to-r from-[#6DC7F9] to-[#0354A5] flex items-center justify-around relative overflow-hidden">
-            <section className="title text-white">
-                <p className="text-[20px]">{text}</p>
-                <h2 className="text-[65px] font-bold">{title}</h2>
+            <section className="title text-white translate-y-10 translate-x-20">
+                <p className="text-[25px] translate-y-3">{text}</p>
+                <h2 className="text-[70px] font-bold">{title}</h2>
             </section>
-            <Image className="absolute top-0 right-50%"
+            <Image className="absolute top-0"
                 src="straight_line1.svg"
                 alt="グラデーションライン"
                 width={738.57}
                 height={93.15}
             />
-            <Image
+            <Image className="-translate-x-5"
                 src={src}
                 alt="メインビジュアル写真"
                 width={820.87}
