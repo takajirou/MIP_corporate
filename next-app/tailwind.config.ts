03af7bfa7273422a9b7393e88d2b305cee4b4d4f
kanayakaisei <kanayakaisei0903@gmail.com>
2025-07-04 14:33:07 +0900

    見出しタイトルサイズと太さ設定

--- next-app/tailwind.config.ts
+++ next-app/tailwind.config.ts
@@ -9,6 +9,11 @@ export default {
                 orange: "#DF7F03",
                 main: "#0354A5",
             },
+            title:{
+                size: "65px",
+                weight: "bold",
+                headingSize: "20px",
+            },
         },
     },
     plugins: [],
