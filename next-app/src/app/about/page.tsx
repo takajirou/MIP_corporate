14af11273762a89abb369325bbde9bb98696da60
kanayakaisei <kanayakaisei0903@gmail.com>
2025-07-05 15:03:08 +0900

    授業時間分

--- next-app/src/app/about/page.tsx
+++ next-app/src/app/about/page.tsx
@@ -1,6 +1,20 @@
 import MainVisual from "@/components/MainVisual";
 import Image from "next/image";
 
+interface OutlineProps{
+    title: string,
+    description: string
+}
+
+const Outline = ({title, description}:OutlineProps) =>{
+    return(
+        <div className="flex text-[#0354A5] text-[20px]  border-b-[#0354A5] border-b-2 mt-[36px] ">
+            <p>{title}</p>
+            <p>{description}</p>
+        </div>
+    )
+}
+
 const About = () => {
     return (
         <>
@@ -17,9 +31,9 @@ const About = () => {
                     そしてチームで成果を生み出すコミュニケーション力を大切にしています。</p>
             </section>
             <section>
-                <section className=" px-[140px] text-[#0354A5]">
+                <section className=" px-[140px] mb-4 text-[#0354A5]">
                     <p className="text-[20px] text-main mt-50">MIPの強み</p>
-                    <h2 className="text-[65px] font-bold text-main mb-4">Strengths</h2>
+                    <h2 className="text-[65px] font-bold text-main">Strengths</h2>
                 </section>
                 <div className="text-[#fff] bg-gradient-to-tr from-[#f28900] to-[#ffcf4a] pb-[100px] pt-[100px] flex flex-col items-center">
                     <div className="flex items-center">
@@ -73,6 +87,21 @@ const About = () => {
                         </section>
                         <Image className="ml-[55px]" src="strengths_img03.svg" alt="一貫支援写真" width={255} height={327}/>
                     </div>
+                </div>
+            </section>
+            <section className=" px-[140px] mb-4 text-[#0354A5]">
+                <p className="text-[20px] text-main mt-50">会社概要</p>
+                <h2 className="text-[65px] font-bold text-main">Outline</h2>
+                <div className="bg-[#fff]">
+                    <section className="py-[55px] px-[228px]">
+                        <Outline title="会社商号" description="株式会社ミップ（MIP Co.,Ltd.）" />
+                        <Outline title="設立年月" description="2000年12月" />
+                        <Outline title="代償取締役" description="成瀬厚司" />
+                        <Outline title="本社所在地" description="〒531-0072大阪市北区豊崎3丁目19番3号 ピアスタワー16F" />
+                        <Outline title="資本金" description="3,500万円" />
+                        <Outline title="事業内容" description="Webソリューション事業" />
+                        <Outline title="取引銀行" description="三井住友銀行(神戸駅前支店)りそな銀行(天六支店)" />
+                    </section>
                 </div>
             </section>
         </>
